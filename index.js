91af059ed18e881c1292921903ab69072c28bdb6
Patrick Franz <clymbing@gmail.com>
2016-08-14 21:45:53 -0700

    Finished first half of data object for profile.

--- index.js
+++ index.js
@@ -19,6 +19,45 @@ var userData = {
       {"Open Source Community" : "Nemo enim ipsam voluptatem quia voluptas sit asperna-tur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione. Neque porro quisquam est, qui dolorem. Sed quia consequuntur magni."},
       {"jQuery Community" : "xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Culpa aut fugit, sed quia qui officia deserunt mollit anim id est laborum."}
     ]
+  },
+
+  biography : {
+    "bigBio" : "Hi folks. I'm Paul. I'm a front-end developer who loves the web. I'm always thinking a lot about how to make you more productive with tools that improve your workflow helpyou make slicker, sexier, mobile web sites and webapps. <br> I work on the Google chrome team focused on Developer Tooling. Chrome DevTools and everything else Chrome can do to make you a productive web designer. <br> I've developed tools like Modernizr, Yeoman, CSS3 Please and HTML5 Boilerplate, curated HTML5 Rocks, and co-created projects like HTML5 Please, motherffingtextshadow, motherffinghsl, Move the Web Forward, Mother-iffing Animated GIF, w3Fools, chromestatus.com, HTML5 Readiness, Front-end Coding Standards, Type Rendering Project.",
+    
+    "trivia" : "I used to host the amazing yayQuery Podcast. I've long loved music and kept the Aurgasm music blog up 10 years.",
+
+    "work-history" : [
+      {
+        "company"    : "TPM for Google Chrome",
+        "position"   : "Developer Advocate",
+        "start-date" : "June 2010",
+        "end-date"   : "Present"
+      },
+      {
+        "company"  : "JQuery Community",
+        "position" : "",
+        "start-date" : "January 2008",
+        "end-date"   : "Present"
+      },
+      {
+        "company"  : "Emerging Interactions",
+        "position" : "Senior Consultant",
+        "start-date" : "October 2006",
+        "end-date"   : "April 2010"
+      },
+      {
+        "company"  : "eInvite",
+        "position" : "Ecommerce Manager",
+        "start-date" : "June 2004",
+        "end-date"   : "July 2006"
+      },
+      {
+        "company"  : "",
+        "position" : "Writing Tutor",
+        "start-date" : "August 2003",
+        "end-date"   : "May 2004"
+      }
+    ]
   }
 
 }
