d8f7a15587b0d12784b8517c904400395d4a66df
Patrick Franz <clymbing@gmail.com>
2016-08-14 21:28:35 -0700

    Add index.js to store user profile as an object.

---
+++ index.js
@@ -0,0 +1,24 @@
+var userData = {
+  shortBio : {
+    "first-name" : "Paul",
+    "last-name"  : "Irish",
+    "about-para" : "American front-end engineer and a developer advocate for Google Chrome, the most popular browser in the world by market share. He is widely recognized as a thought leader and a leading evangelist in web technologies, including JavaScript and CSS. He was named Developer of the Year by The Net Awards in 2014. ",
+    "current-project"     : "Aurgasm",
+    "current-project-url" : "http://aurgasm.us/",
+    "contact-links" : [
+      {"location" : "Silicon Valley, CA"},
+      {"twitter"  : "@paul_irish"},
+      {"github"   : "https://github.com/paulirish"},
+      {"website"  : "http://www.paulirish.com/"},
+      {"blog"     : "http://www.paulirish.com/"},
+      {"Company"  : "https://www.google.com"}
+    ]
+  },
+  contributions : {
+    [
+      {"Open Source Community" : "Nemo enim ipsam voluptatem quia voluptas sit asperna-tur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione. Neque porro quisquam est, qui dolorem. Sed quia consequuntur magni."},
+      {"jQuery Community" : "xcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Culpa aut fugit, sed quia qui officia deserunt mollit anim id est laborum."}
+    ]
+  }
+
+}
